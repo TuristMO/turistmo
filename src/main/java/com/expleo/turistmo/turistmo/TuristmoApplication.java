3066e77dbe34d9b183a04a8783402b5b42aa1ff8
RikardLof <rikardlof@gmail.com>
2020-09-18 16:57:40 +0200

    first commit

---
+++ src/main/java/com/expleo/turistmo/turistmo/TuristmoApplication.java
@@ -0,0 +1,13 @@
+package com.expleo.turistmo.turistmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expleo/turistmo/turistmo/TuristmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expleo.turistmo.turistmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
