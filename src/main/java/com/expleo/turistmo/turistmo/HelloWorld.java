2323d29cf9a4ac8516f5418abf70a04750582048
fazlizekiqi <fazlizekiqi1@hotmail.com>
2020-09-18 17:44:19 +0200

    Added static file together with a test class.

---
+++ src/main/java/com/expleo/turistmo/turistmo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.expleo.turistmo.turistmo;
+
+public class HelloWorld {
+
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/expleo/turistmo/turistmo/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.expleo.turistmo.turistmo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    HelloWorld helloWorld = new HelloWorld();
+
+    @Test
+    void helloWorld() {
+        assertThat(helloWorld.helloWorld()).isNotNull();
+    }
+
+}
