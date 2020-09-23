8116193aeb60726c85c876d02719eeba6fb09742
Atef Sendesni <atefsendesni512@hotmail.com>
2020-09-23 15:44:25 +0200

    Added more object in mock-data

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,45 +1,61 @@
-import React, { useState } from 'react';
-import { View, Text, ScrollView, TextInput, FlatList } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, ScrollView, TextInput, FlatList} from 'react-native';
 import {Input, Button} from "react-native-elements";
 
-const app = [{
-    "name": "SL",
-    "country": "Stockholm",
-},
+const app = [
+    {
+        "name": "SL",
+        "city": "Stockholm",
+    },
+    {
+        "name": "Skåne-trafik",
+        "city": "Malmö",
+    }, {
+        "name": "SJ",
+        "city": "Stockholm",
+    }, {
+        "name": "Taxi Stockholm",
+        "city": "Stockholm",
+    }, {
+        "name": "Taxi-020",
+        "city": "Stockholm",
+    },
     {
         "name": "Region väst-trafiken",
-        "country": "Göteborg",
-    }]
+        "city": "Göteborg",
+    }
+]
 
 const App = () => {
-  const [text, setText] = useState('');
-  const [filteredData,setFilteredData] = useState([]);
-  const searchResult = (searchItem) => {
-     const searchData = app.filter(item => item.country===searchItem)
-      setFilteredData(searchData);
-  }
-  return (
-      <View style={{padding: 50}}>
-        <Input
-            style={{height: 40}}
-            onChangeText={text => searchResult(text)}
-            defaultValue="Enter search"
-        />
-        <FlatList
-            data={filteredData}
-            keyExtractor={(item)=>item.name}
-            renderItem={({item})=>{
-                return <Text style={{marginBottom:20}}>
-                    {item.name}
-                </Text>
-            }}
-        />
+    const [text, setText] = useState('');
+    const [filteredData, setFilteredData] = useState([]);
+    const searchResult = (searchItem) => {
+        const searchData = app.filter(item => item.city.toLowerCase().startsWith(searchItem.toLowerCase()))
+        setFilteredData(searchData);
+    }
+    return (
+        <View style={{padding: 50}}>
+            <Input
+                style={{height: 40}}
+                onChangeText={text => setText(text)}
+                placeholder="Enter search"
+            />
+            <Button
+                title="Search"
+                onPress={() => searchResult(text)}
+            />
+            <FlatList
+                data={filteredData}
+                keyExtractor={(item) => item.name}
+                renderItem={({item}) => {
+                    return <Text style={{marginBottom: 20}}>
+                        {item.name}
+                    </Text>
+                }}
+            />
 
-        <Text style={{padding: 10, fontSize: 42}}>
-          {text}
-        </Text>
-      </View>
-  );
+        </View>
+    );
 }
 
 export default App;
