20167a6f996d94722223d690222918927139c8e7
fazlizekiqi <fazlizekiqi1@hotmail.com>
2020-09-28 11:26:49 +0200

    Deleted appium imports from tests

--- src/test/java/com/expleo/turistmo/turistmo/HelloWorldTest.java
+++ src/test/java/com/expleo/turistmo/turistmo/HelloWorldTest.java
@@ -1,17 +1,7 @@
 package com.expleo.turistmo.turistmo;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.MobileCapabilityType;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 class HelloWorldTest {
 
@@ -21,5 +11,5 @@ class HelloWorldTest {
     void helloWorld() {
         assertThat(helloWorld.helloWorld()).isNotNull();
     }
-    
+
 }
