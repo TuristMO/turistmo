e608ce83175b5236ef5e16a84163f8f4c870faf4
fazlizekiqi <fazlizekiqi1@hotmail.com>
2020-09-28 15:03:18 +0200

    Added android && ios configuration in the pipeline

---
+++ mobileApp/detox.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  configurations: {
+    "android.emu.debug": {
+      binaryPath: "android/app/build/outputs/apk/debug/app-debug.apk",
+      build: "cd android && ./gradlew app:assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..",
+      type: "android.emulator",
+      device: {
+        avdName: "Pixel_3_API_27",
+      },
+    },
+    "android.emu.release": {
+      binaryPath: "android/app/build/outputs/apk/release/app-release.apk",
+      build: "cd android && ./gradlew app:assembleRelease assembleAndroidTest -DtestBuildType=release && cd ..",
+      type: "android.emulator",
+      device: {
+        avdName: "Pixel_3_API_27",
+      },
+    },
+    "android": {
+      binaryPath: "android/app/build/outputs/apk/release/app-release.apk",
+      build: "cd android && ./gradlew app:assembleRelease assembleAndroidTest -DtestBuildType=release && cd ..",
+      type: "android.emulator",
+      device: {
+        avdName: "Pixel_3_API_27",
+      },
+    },
+
+    "ios.sim.release": {
+      binaryPath: "ios/build/Build/Products/Release-iphonesimulator/mobileApp.app",
+      build: "export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild ONLY_ACTIVE_ARCH=YES -workspace ios/mobileApp.xcworkspace -scheme mobileApp -configuration Release -sdk iphonesimulator -derivedDataPath ios/build",
+      type: "ios.simulator",
+      device: {
+        type: "iPhone 11 Pro",
+      },
+    },
+    "ios.sim.debug": {
+      binaryPath: "ios/build/Build/Products/Debug-iphonesimulator/mobileApp.app",
+      build: "xcodebuild -workspace ios/mobileApp.xcworkspace -scheme mobileApp -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build",
+      type: "ios.simulator",
+      device: {
+        type: "iPhone 11 Pro",
+      },
+    },
+    "ios": {
+      binaryPath: "ios/build/Build/Products/Debug-iphonesimulator/mobileApp.app",
+      build: "xcodebuild -workspace ios/mobileApp.xcworkspace -scheme mobileApp -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build",
+      type: "ios.simulator",
+      device: {
+        type: "iPhone 11 Pro",
+      },
+    },
+  },
+  "test-runner": "jest",
+  runnerConfig: "e2e/config.json",
+  behavior: {
+    init: { launchApp: true },
+  },
+
+};
