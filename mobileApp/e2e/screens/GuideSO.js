903a97600edb4d01d95e9fe41bda40adc58fe169
RikardLof <rikardlof@gmail.com>
2020-11-03 11:17:49 +0100

    Removed detox test 3.6, changed 2.x

--- mobileApp/e2e/screens/GuideSO.js
+++ mobileApp/e2e/screens/GuideSO.js
@@ -1,5 +1,6 @@
 import {Utility} from "../Utility";
 import {AppSO} from "./AppSO";
+import {sleep} from "../helpers";
 
 const {device, expect, element, by, waitFor} = require('detox');
 
@@ -8,16 +9,19 @@ export class GuideSO extends Utility {
     async clickInstallButton(timeout) {
         await this.toBeVisibleById("guideInstallButton", timeout);
         await this.tapById("guideInstallButton");
+        await sleep(2000);
     }
 
     async clickDeleteButton(timeout) {
         await this.toBeVisibleById("guideDeleteButton", timeout);
         await this.tapById("guideDeleteButton");
+        await sleep(2000);
     }
 
     async clickCuratorButton(timeout) {
         await this.toBeVisibleById("guideCuratorButton", timeout);
         await this.tapById("guideCuratorButton");
+        await sleep(2000);
     }
 
 }
