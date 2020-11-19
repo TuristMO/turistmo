7139b0c654aae3aa7dd4bd478466478e651da651
Soren-S <soren.v.stahl@gmail.com>
2020-11-19 13:46:13 +0100

    5.6 fix and optimizing

--- mobileApp/e2e/testcases/5_curator/5.6.e2e.js
+++ mobileApp/e2e/testcases/5_curator/5.6.e2e.js
@@ -26,23 +26,16 @@ describe('TuristMO', () => {
 
         await appSO.tapCuratorTab();
         await splashSO.tapGetStartedButton();
+        await signinSO.fillPassword("123456");
+        await signinSO.tapById("signinShowHideToggle");
+        await signinSO.waitToHaveTextById("signinPassword", "123456");
         await signinSO.tapSignupButton();   //Also expects visibility
-        await signupSO.fillEmail("minemailadress@domain.co.uk");
-        await signupSO.waitToHaveTextById("signupEmail", "minemailadress@domain.co.uk");
         await signupSO.fillPassword("123456");
         await signupSO.fillConfirmPassword("123456");
         await signupSO.tapById("signupShowHideToggle");
         await signupSO.waitToHaveTextById("signupPassword", "123456");
         await signupSO.waitToHaveTextById("confirmPassword", "123456");
 
-        await signupSO.tapGoBackArrow();
-
-        await splashSO.tapGetStartedButton();  //Also expects visibility
-        await signinSO.fillSigninEmail("minemailadress@domain.co.uk");
-        await signupSO.waitToHaveTextById("signinEmail", "minemailadress@domain.co.uk");
-        await signinSO.fillPassword("123456");
-        await signinSO.tapById("signinShowHideToggle");
-        await signinSO.waitToHaveTextById("signinPassword", "123456");
 
     });
 
